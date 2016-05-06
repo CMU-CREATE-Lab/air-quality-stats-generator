21facac5c897fde9e6218ac1db80de4c57762327
Chris Bartley <bartley@cmu.edu>
2016-05-06 15:40:21 -0400

    Added -d switch for computing stats only for the past N days. Also now deleting data and stats dirs on startup.

--- index.js
+++ index.js
@@ -5,42 +5,86 @@ if (!RunMode.isValid()) {
 }
 
 var config = require('./config');
-var FeedDownloader = require('./FeedDownloader');
-var StatsGenerator = require('./StatsGenerator');
-var FeedImporter = require('./FeedImporter');
+var pjson = require('./package.json');
+var Common = require('./Common.js');
+var path = require('path');
+var program = require('commander');
+program
+      .version(pjson.version)
+      .option('-d, --days <N>', 'Compute stats for the past N days', parseInt)
+      .parse(process.argv);
 
-var downloader = new FeedDownloader();
-downloader.download(null, function(err, results) {
-   if (err) {
-      console.log(err);
+// validate the days param (if specified at all)
+if (typeof program.days === 'undefined' || (program.days != null && program.days > 0)) {
+   var startDateUnixTimeSecs = null;
+
+   // if the user specified the days param, then compute the starting date in Unix time secs
+   if (typeof program.days !== 'undefined') {
+      var today = new Date();
+      today.setUTCHours(0, 0, 0, 0);
+      today.setUTCDate(today.getUTCDate() - program.days);
+      startDateUnixTimeSecs = today.getTime() / 1000;
    }
-   else {
-      console.log("Downloaded " + results.length + " feeds.");
 
-      var statsGenerator = new StatsGenerator();
-      statsGenerator.generate(function(err, results) {
+   // delete any existing data and stats directories
+   var deleteDir = require('rimraf');
+   deleteDir(Common.DATA_DIRECTORY, function(err) {
+      if (err) {
+         console.log("ERROR: failed to delete data directory [" + Common.DATA_DIRECTORY + "]. Aborting.");
+         process.exit(1);
+      }
+
+      deleteDir(Common.STATS_DIRECTORY, function(err) {
          if (err) {
-            console.log(err);
+            console.log("ERROR: failed to delete stats directory [" + Common.STATS_DIRECTORY + "]. Aborting.");
+            process.exit(1);
          }
-         else {
-            console.log("Generated stats for " + results.length + " feeds.");
 
-            var feedImporter = new FeedImporter({
-               userId : config.get("esdr:feedOwnerUserId"),
-               datastore : config.get("datastore")
-            });
+         // Start the downloader
+         var FeedDownloader = require('./FeedDownloader');
+         var downloader = new FeedDownloader();
+         downloader.download(startDateUnixTimeSecs, function(err, results) {
+            if (err) {
+               console.log(err);
+            }
+            else {
+               console.log("Downloaded " + results.length + " feeds.");
 
-            feedImporter.import(function(err, results) {
-               if (err) {
-                  console.log(err);
-               }
-               else {
-                  console.log("All done! Imported " + results.length + " feeds.");
-               }
-            });
-         }
+               // Start the stats generator
+               var StatsGenerator = require('./StatsGenerator');
+               var statsGenerator = new StatsGenerator();
+               statsGenerator.generate(function(err, results) {
+                  if (err) {
+                     console.log(err);
+                  }
+                  else {
+                     console.log("Generated stats for " + results.length + " feeds.");
+
+                     // Import the stats back into the feeds
+                     var FeedImporter = require('./FeedImporter');
+                     var feedImporter = new FeedImporter({
+                        userId : config.get("esdr:feedOwnerUserId"),
+                        datastore : config.get("datastore")
+                     });
+
+                     feedImporter.import(function(err, results) {
+                        if (err) {
+                           console.log(err);
+                        }
+                        else {
+                           console.log("All done! Imported " + results.length + " feeds.");
+                        }
+                     });
+                  }
+               });
+            }
+         });
       });
-   }
-});
+   });
+}
+else {
+   console.log("ERROR: Number of days must be a positive integer.");
+   process.exit(1);
+}
 
 
