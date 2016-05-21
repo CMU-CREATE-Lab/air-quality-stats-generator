532d5a16e6cc5db3ae9daae7c653660c64f535f0
Chris Bartley <bartley@cmu.edu>
2016-05-21 12:34:54 -0400

    Added script which will be called by cron in production

---
+++ generate-air-quality-stats.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# cd to the directory containing the index.js (found this at http://stackoverflow.com/a/3355423/703200)
+cd "$(dirname "$0")"
+
+NODE_ENV=prod /usr/local/bin/node index.js -d 60;
