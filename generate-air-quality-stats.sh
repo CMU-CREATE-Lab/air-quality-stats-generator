cb7f51204e88c19e152e965109b807d48baae651
Chris Bartley <bartley@cmu.edu>
2017-10-16 09:31:10 -0400

    Fixed path to node for new ESDR

--- generate-air-quality-stats.sh
+++ generate-air-quality-stats.sh
@@ -3,4 +3,4 @@
 # cd to the directory containing the index.js (found this at http://stackoverflow.com/a/3355423/703200)
 cd "$(dirname "$0")"
 
-NODE_ENV=prod /usr/local/bin/node index.js -d 60;
+NODE_ENV=prod /home/node/n/bin/node index.js -d 60;
